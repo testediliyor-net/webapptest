9f943b28e73dcd908f63ec9f261af5f04dcc1e22
Kerem Yavuz <kerem.yavuz.in@gmail.com>
2024-05-31 00:40:29 +0300

    updated

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Merhaba Dünya!');
+app.get("/", (req, res) => {
+  res.send("Merhaba Dünya!");
 });
 
 app.listen(port, () => {
